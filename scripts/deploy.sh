875e2675de228fd39dc5d0fa1f7d4e0b33d4ccd2
Seth Saulnier <sethsaulnier@saulnet.com>
2023-04-13 09:05:17 -0400

    install runs through

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,23 +7,26 @@ sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install lib32gcc-s1 steamcmd apache2
 
-cd /home/steam && git clone https://github.com/smsaul/valheim-server.git
-
-mkdir /home/steam/.config/unity3d/IronGate/Valheim/worlds_local
-
+echo "Linking steamcmd executable..."
 ln -s /usr/games/steamcmd /home/steam/valheim-server/scripts/steamcmd
+echo "Creating worlds folder..."
+mkdir -p /home/steam/.config/unity3d/IronGate/Valheim/worlds_local
+
+echo "Installing Valheim..."
 /home/steam/valheim-server/scripts/InstallUpdate.sh
 
+echo "Firewall settings..."
 sudo ufw allow 2346
 sudo ufw allow 2347
 sudo ufw allow 'Apache Full'
 
+
+echo "Configuring web server..."
 sudo cp /home/steam/valheim-server/web/index.html /var/www/html
 sudo mkdir /var/www/html/downloads
 
+echo "Creating logs..."
 mkdir /home/steam/logs
 ln -s /home/steam/.steam/logs/connection_log_2346.txt /home/steam/logs/connections.txt
 
-read -p 'Press Enter to reboot...' prompt
-
-sudo reboot
+echo "You should really reboot."
