b26d50c64b35c098886a44dde70427950509fafd
Seth Saulnier <seth.saulnier@icloud.com>
2023-04-14 01:01:55 -0400

    removed `screen` execution within script

--- scripts/valheim.sh
+++ scripts/valheim.sh
@@ -4,5 +4,5 @@ export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppID=892970
 
 echo "Starting server PRESS CTRL-C to exit"  
-screen /home/steam/valheim/valheim_server.x86_64 -name "world" -port 2456 -nographics -batchmode -world "world" -password "world" -public 1 >> /home/steam/logs/serverlog-$(date -I).txt
+/home/steam/valheim/valheim_server.x86_64 -name "world" -port 2456 -nographics -batchmode -world "world" -password "world" -public 1 >> /home/steam/logs/serverlog-$(date -I).txt
 export LD_LIBRARY_PATH=$templdpath
